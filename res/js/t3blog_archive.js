e36f75d82d12d8d59c671812909a53e62f3a865a
acquire <acquire@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-06-13 10:03:02 +0000

    [#BUG] Updated JavaScripts because of id changes in t3blog 1.1.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3blogjquery/trunk@48651 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/t3blog_archive.js
+++ res/js/t3blog_archive.js
@@ -1,23 +1,23 @@
 jQuery(document).ready(function() {
-	jQuery("a[id^='toggle']").each(function () {
+	jQuery("#archive_ a[id^='toggle']").each(function () {
 		var year = this.id.substr(6);
-    if(jQuery.cookie('archive_'+year)=='1') {
-      jQuery('#archive_'+year).slideUp();
-      jQuery('#'+this.id).text('[+]');
-    } else {
-      jQuery('#'+this.id).text('[-]');
-    }
-    jQuery('#'+this.id).click(function() {
-      if(jQuery('#'+this.id).text()=='[-]') {
-        jQuery('#archive_'+year).slideUp();
-        jQuery.cookie('archive_'+year,'1',{ path:'/'});
-        jQuery('#'+this.id).text('[+]');
-      } else {
-        jQuery('#archive_'+year).slideDown();
-        jQuery.cookie('archive_'+year,'0',{ path:'/'});
-        jQuery('#'+this.id).text('[-]');
-      }
-      return false;
-    });
-  });
+		if(jQuery.cookie('archive_'+year)=='1') {
+			jQuery('#archive_'+year).slideUp();
+			jQuery('#'+this.id).text('[+]');
+		} else {
+			jQuery('#'+this.id).text('[-]');
+		}
+		jQuery('#'+this.id).click(function() {
+			if(jQuery('#'+this.id).text()=='[-]') {
+				jQuery('#archive_'+year).slideUp();
+				jQuery.cookie('archive_'+year,'1',{ path:'/'});
+				jQuery('#'+this.id).text('[+]');
+			} else {
+				jQuery('#archive_'+year).slideDown();
+				jQuery.cookie('archive_'+year,'0',{ path:'/'});
+				jQuery('#'+this.id).text('[-]');
+			}
+			return false;
+		});
+	});
 });
--- res/js/t3blog_cat.js
+++ res/js/t3blog_cat.js
@@ -1,23 +1,23 @@
 jQuery(document).ready(function() {
-	jQuery("a[id^='imgtogglecat']").each(function () {
-		var catNo = this.id.substr(12);
-    if(jQuery.cookie('togglecat'+catNo)=='1') {
-      jQuery('#togglecat'+catNo).slideUp();
-      jQuery('#'+this.id).text('[+]');
-    } else {
-      jQuery('#'+this.id).text('[-]');
-    }
-    jQuery('#'+this.id).click(function() {
-      if(jQuery('#'+this.id).text()=='[-]') {
-        jQuery('#togglecat'+catNo).slideUp();
-        jQuery.cookie('togglecat'+catNo,'1',{ path:'/'});
-        jQuery('#'+this.id).text('[+]');
-      } else {
-        jQuery('#togglecat'+catNo).slideDown();
-        jQuery.cookie('togglecat'+catNo,'0',{ path:'/'});
-        jQuery('#'+this.id).text('[-]');
-      }
-      return false;
-    });
-  });
+	jQuery("a[id^='togglecat']").each(function () {
+		var catId = this.id.substr(6);
+		if(jQuery.cookie(catId)=='1') {
+			jQuery('#'+catId).slideUp();
+			jQuery('#'+this.id).text('[+]');
+		} else {
+			jQuery('#'+this.id).text('[-]');
+		}
+		jQuery('#'+this.id).click(function() {
+			if(jQuery('#'+this.id).text()=='[-]') {
+				jQuery('#'+catId).slideUp();
+				jQuery.cookie(catId,'1',{ path:'/'});
+				jQuery('#'+this.id).text('[+]');
+			} else {
+				jQuery('#'+catId).slideDown();
+				jQuery.cookie(catId,'0',{ path:'/'});
+				jQuery('#'+this.id).text('[-]');
+			}
+			return false;
+		});
+	});
 });
